2304a63b238d5c3effd4908050e03a112f981733
Rybka <112930666+Rybk4@users.noreply.github.com>
2023-09-14 20:24:58 +0600

    а

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,7 +1,24 @@
+import logo from '../assets/white-logo.png';
+import { Link } from 'react-router-dom';
+
 const Nav = () => {
     return (
-        <div className="flex justify-center w-full bg-red-300">
-            <h1 className="text-4xl">This is nav</h1>
+        <div className="flex justify-center items-center " >
+            <div className="flex justify-between w-4/5 py-6">
+                <a href="/"> <img  src={logo} alt="n" className="w-38 h-10 "/></a>
+                <div className="flex justify-between space-x-12 text-[#ffffff] py-2 items-center text-2xl ">
+                    <a href='#' className="">Обзор</a>
+                    <a href='#' className=" ">Советы</a>
+                    <a href='#' className=" ">Оповещения</a>
+                    <a href='#' className=" ">Блог</a>
+                    <Link
+                    to="/"
+                    className="bg-[#ffffff] text-[#070908]   px-4 py-2 rounded hover:bg-[#597367] transition duration-300 ease-in-out "
+                    >
+                        Блог
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
