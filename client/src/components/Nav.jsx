4de4431a4604e6130b3049d86670bff0abcaae8f
makkenzo <nekgo2009@gmail.com>
2023-09-14 20:31:59 +0600

    fix

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,17 +3,20 @@ import { Link } from 'react-router-dom';
 
 const Nav = () => {
     return (
-        <div className="flex justify-center items-center " >
+        <div className="flex justify-center items-center ">
             <div className="flex justify-between w-4/5 py-6">
-                <a href="/"> <img  src={logo} alt="n" className="w-38 h-10 "/></a>
+                <a href="/">
+                    {' '}
+                    <img src={logo} alt="n" className="w-38 h-10 " />
+                </a>
                 <div className="flex justify-between space-x-12 text-[#ffffff] py-2 items-center text-2xl ">
-                    <a href='#' className="">Обзор</a>
-                    <a href='#' className=" ">Советы</a>
-                    <a href='#' className=" ">Оповещения</a>
-                    <a href='#' className=" ">Блог</a>
+                    <a href="#">Обзор</a>
+                    <a href="#">Советы</a>
+                    <a href="#">Оповещения</a>
+                    <a href="#">Блог</a>
                     <Link
-                    to="/"
-                    className="bg-[#ffffff] text-[#070908]   px-4 py-2 rounded hover:bg-[#597367] transition duration-300 ease-in-out "
+                        to="/"
+                        className="bg-[#ffffff] text-[#070908]   px-4 py-2 rounded hover:bg-[#597367] transition duration-300 ease-in-out "
                     >
                         Блог
                     </Link>
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -5,19 +5,21 @@ import bg from '../assets/bg.png';
 const HomePage = () => {
     return (
         <>
-        <div className="w-full bg-cover bg-center" style={{backgroundImage:`url(${bg})`}}>
-            <Nav />
-            
-            <div className='text-[#ffffff]'>
-                <h1 className=" flex justify-center text-center text-6xl mt-48">
-                    Откройте Для Себя Свою Жизнь,<br /> 
-                    Путешествуйте  Куда Хотите</h1>
-                <p className='text-2xl flex justify-center text-center mt-6 pb-72'>
-                    Хотите ли вы исследовать природный рай в мире, давайте вместе с нами <br />
-                    найдем лучшее место в мире.</p>
+            <div className="w-full bg-cover bg-center" style={{ backgroundImage: `url(${bg})` }}>
+                <Nav />
+
+                <div className="text-[#ffffff]">
+                    <h1 className=" flex justify-center text-center text-6xl mt-48">
+                        Откройте Для Себя Свою Жизнь,
+                        <br />
+                        Путешествуйте Куда Хотите
+                    </h1>
+                    <p className="text-2xl flex justify-center text-center mt-6 pb-72">
+                        Хотите ли вы исследовать природный рай в мире, давайте вместе с нами <br />
+                        найдем лучшее место в мире.
+                    </p>
+                </div>
             </div>
-        </div>
-           
         </>
     );
 };
