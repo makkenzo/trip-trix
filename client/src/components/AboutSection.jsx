67f48bd681f3961310d200f96753554a24d7381f
makkenzo <nekgo2009@gmail.com>
2023-11-20 08:29:23 +0600

    asd

--- client/src/components/AboutSection.jsx
+++ client/src/components/AboutSection.jsx
@@ -1,44 +1,50 @@
-import React from 'react'
+import React from 'react';
 import bg from '../assets/AboutImage.png';
 const AboutSection = () => {
-  return (
-    <div className='flex justify-around mt-[80px]'> 
-        <div>
-            <div className='flex justify-around'>
-                <div><img src={bg} alt="2" srcset="" /></div>
-                <div>
-                    <div className='text-center p-3 w-[180px] h-[120] mt-[56px]'>
-                        <h1 className='text-sky-600 font-bold'>300+</h1>
-                        <p>НАПРАВЛЕНИЙ</p>
+    return (
+        <div className="flex justify-around mt-[80px]">
+            <div>
+                <div className="flex justify-around">
+                    <div>
+                        <img src={bg} alt="2" srcset="" />
                     </div>
-                    <div className='text-center p-3 w-[180px] h-[120] mt-[200px]'>
-                        <h1 className='text-sky-600 font-bold'>5000+</h1>
-                        <p>ТУРОВ</p>
-                    </div>
-                    <div className='text-center p-3 w-[180px] h-[120] mt-[200px]'>
-                        <h1 className='text-sky-600 font-bold'>150+</h1>
-                        <p>ОТЕЛЕЙ</p>
+                    <div>
+                        <div className="text-center p-3 w-[180px] h-[120] mt-[56px]">
+                            <h1 className="text-sky-600 font-bold">300+</h1>
+                            <p>НАПРАВЛЕНИЙ</p>
+                        </div>
+                        <div className="text-center p-3 w-[180px] h-[120] mt-[200px]">
+                            <h1 className="text-sky-600 font-bold">5000+</h1>
+                            <p>ТУРОВ</p>
+                        </div>
+                        <div className="text-center p-3 w-[180px] h-[120] mt-[200px]">
+                            <h1 className="text-sky-600 font-bold">150+</h1>
+                            <p>ОТЕЛЕЙ</p>
+                        </div>
                     </div>
                 </div>
-             </div>
-        </div>
-           
-        <div>
-            <div className='mt-[100px]'>
-                <h1 className='font-bold'>Travel Any Corner of <br/>
-                    The World With Us</h1>
-                <p>Would you explore nature paradise in the world, let’s find <br />
-                the best destination in world with us, Would you explore <br />
-                nature paradise in the world, let’s find the best destination <br />
-                in world with us. Would you explore nature paradise in the <br />
-                world, let’s find the best destination in world with us. <br /><br />
-                Would you explore nature paradise in the world, let’s find <br />
-                the best destination in world with us.</p>
-                
+            </div>
+
+            <div>
+                <div className="mt-[100px]">
+                    <h1 className="font-bold">
+                        Travel Any Corner of <br />
+                        The World With Us
+                    </h1>
+                    <p>
+                        Would you explore nature paradise in the world, let’s find <br />
+                        the best destination in world with us, Would you explore <br />
+                        nature paradise in the world, let’s find the best destination <br />
+                        in world with us. Would you explore nature paradise in the <br />
+                        world, let’s find the best destination in world with us. <br />
+                        <br />
+                        Would you explore nature paradise in the world, let’s find <br />
+                        the best destination in world with us.
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default AboutSection
+export default AboutSection;
