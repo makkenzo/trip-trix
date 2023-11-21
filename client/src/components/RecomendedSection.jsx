1fdbe9f39c05638d896c6d4b6eba75d21a774ce9
Rybka <nikitarybalkin1@gmail.com>
2023-11-21 09:52:03 +0600

    a

--- client/src/components/RecomendedSection.jsx
+++ client/src/components/RecomendedSection.jsx
@@ -5,6 +5,9 @@ const RecomendedSection = () => {
     <div className='container mx-auto flex my-12  justify-center'>
         <h1 className='font-bold text-5xl mt-[150px]'>Рекомендуемые Места</h1>
     </div>
+
+
+
   )
 }
 
