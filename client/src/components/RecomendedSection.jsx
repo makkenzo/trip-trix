3572cad2e5f8be71519080a9eef91a30f4bdf3c6
makkenzo <nekgo2009@gmail.com>
2023-11-21 10:07:59 +0600

    tabs

--- client/src/components/RecomendedSection.jsx
+++ client/src/components/RecomendedSection.jsx
@@ -1,14 +1,60 @@
-import React from 'react'
+import { Tabs, TabsHeader, TabsBody, Tab, TabPanel } from '@material-tailwind/react';
+
+const data = [
+    {
+        label: 'HTML',
+        value: 'html',
+        desc: `It really matters and then like it really doesn't matter.
+      What matters is the people who are sparked by it. And the people 
+      who are like offended by it, it doesn't matter.`,
+    },
+    {
+        label: 'React',
+        value: 'react',
+        desc: `Because it's about motivating the doers. Because I'm here
+      to follow my dreams and inspire other people to follow their dreams, too.`,
+    },
+    {
+        label: 'Vue',
+        value: 'vue',
+        desc: `We're not always in the position that we want to be at.
+      We're constantly growing. We're constantly making mistakes. We're
+      constantly trying to express ourselves and actualize our dreams.`,
+    },
+    {
+        label: 'Angular',
+        value: 'angular',
+        desc: `Because it's about motivating the doers. Because I'm here
+      to follow my dreams and inspire other people to follow their dreams, too.`,
+    },
+    {
+        label: 'Svelte',
+        value: 'svelte',
+        desc: `We're not always in the position that we want to be at.
+      We're constantly growing. We're constantly making mistakes. We're
+      constantly trying to express ourselves and actualize our dreams.`,
+    },
+];
 
 const RecomendedSection = () => {
-  return (
-    <div className='container mx-auto flex my-12  justify-center'>
-        <h1 className='font-bold text-5xl mt-[150px]'>Рекомендуемые Места</h1>
-    </div>
+    return (
+        <Tabs value="html">
+            <TabsHeader>
+                {data.map(({ label, value }) => (
+                    <Tab key={value} value={value}>
+                        {label}
+                    </Tab>
+                ))}
+            </TabsHeader>
+            <TabsBody>
+                {data.map(({ value, desc }) => (
+                    <TabPanel key={value} value={value}>
+                        {desc}
+                    </TabPanel>
+                ))}
+            </TabsBody>
+        </Tabs>
+    );
+};
 
-
-
-  )
-}
-
-export default RecomendedSection
+export default RecomendedSection;
--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -8,7 +8,7 @@ const ErrorPage = () => {
                 <p className=" mb-6 text-[#070908]">Извините, запрашиваемая вами страница не существует.</p>
                 <Link
                     to="/"
-                    className="bg-[#769386] text-[#070908] px-4 py-2 rounded hover:bg-[#597367] transition duration-300 ease-in-out"
+                    className="bg-[#C6D2CD] text-[#070908] px-4 pt-2 pb-3 rounded hover:bg-[#769386] hover:text-white text-xl transition duration-300 ease-in-out"
                 >
                     На главную
                 </Link>
