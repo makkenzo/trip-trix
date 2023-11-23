043c8af29301b0ed719d6d23affff423250f4c16
makkenzo <nekgo2009@gmail.com>
2023-11-23 09:15:53 +0600

    upd

--- client/src/components/MainInput.jsx
+++ client/src/components/MainInput.jsx
@@ -1,53 +1,63 @@
 import React from 'react';
-import { FaPlane } from 'react-icons/fa6';
+import { FaPlane, FaCalendarDays } from 'react-icons/fa6';
 import { Select, Option } from '@material-tailwind/react';
 
 const MainInput = () => {
     return (
-        <div className="container mx-auto flex">
-            <div className="border border-black border-dashed p-4 flex">
+        <div className="container mx-auto flex mt-16 space-x-4">
+            <div className="border border-gray-500 p-4 flex items-center rounded-2xl space-x-4">
                 <div>
-                    <p>Город вылета:</p>
-                    <Select color="blue" label="Select Version">
-                        <Option>Material Tailwind HTML</Option>
-                        <Option>Material Tailwind React</Option>
-                        <Option>Material Tailwind Vue</Option>
-                        <Option>Material Tailwind Angular</Option>
-                        <Option>Material Tailwind Svelte</Option>
+                    <p className="text-gray-600 mb-2">Город вылета</p>
+                    <Select label="Откуда" className="font-bold">
+                        <Option className="font-bold">Material Tailwind HTML</Option>
+                        <Option className="font-bold">Material Tailwind React</Option>
+                        <Option className="font-bold">Material Tailwind Vue</Option>
+                        <Option className="font-bold">Material Tailwind Angular</Option>
+                        <Option className="font-bold">Material Tailwind Svelte</Option>
                     </Select>
                 </div>
-                <FaPlane />
+                <div className="border p-2 border-black border-dashed rounded-full">
+                    <FaPlane size={20} color="#757575" />
+                </div>
                 <div>
-                    <p>Страна прилета:</p>
-                    <Select color="blue" label="Select Version">
-                        <Option>Material Tailwind HTML</Option>
-                        <Option>Material Tailwind React</Option>
-                        <Option>Material Tailwind Vue</Option>
-                        <Option>Material Tailwind Angular</Option>
-                        <Option>Material Tailwind Svelte</Option>
+                    <p className="text-gray-600 mb-2">Страна прилета</p>
+                    <Select label="Куда" className="font-bold">
+                        <Option className="font-bold">Material Tailwind HTML</Option>
+                        <Option className="font-bold">Material Tailwind React</Option>
+                        <Option className="font-bold">Material Tailwind Vue</Option>
+                        <Option className="font-bold">Material Tailwind Angular</Option>
+                        <Option className="font-bold">Material Tailwind Svelte</Option>
                     </Select>
                 </div>
             </div>
-            <div className="border border-black border-dashed p-4">
+            <div className="border border-gray-500 p-4 flex items-center rounded-2xl space-x-4">
                 <div>
-                    <p>Город вылета:</p>
-                    <Select color="blue" label="Select Version">
-                        <Option>Material Tailwind HTML</Option>
-                        <Option>Material Tailwind React</Option>
-                        <Option>Material Tailwind Vue</Option>
-                        <Option>Material Tailwind Angular</Option>
-                        <Option>Material Tailwind Svelte</Option>
+                    <div className="flex justify-between items-center mb-2">
+                        <p className="text-gray-600 ">Дата вылета</p>
+                        <FaPlane size={20} color="#769386" />
+                    </div>
+                    <Select label="Откуда" className="font-bold">
+                        <Option className="font-bold">Material Tailwind HTML</Option>
+                        <Option className="font-bold">Material Tailwind React</Option>
+                        <Option className="font-bold">Material Tailwind Vue</Option>
+                        <Option className="font-bold">Material Tailwind Angular</Option>
+                        <Option className="font-bold">Material Tailwind Svelte</Option>
                     </Select>
                 </div>
-                <FaPlane />
+                <div className="border p-2 border-black border-dashed rounded-full">
+                    <FaCalendarDays size={20} color="#757575" />
+                </div>
                 <div>
-                    <p>Страна прилета:</p>
-                    <Select color="blue" label="Select Version">
-                        <Option>Material Tailwind HTML</Option>
-                        <Option>Material Tailwind React</Option>
-                        <Option>Material Tailwind Vue</Option>
-                        <Option>Material Tailwind Angular</Option>
-                        <Option>Material Tailwind Svelte</Option>
+                    <div className="flex justify-between items-center mb-2">
+                        <p className="text-gray-600 ">Дата прилета</p>
+                        <FaPlane size={20} color="#769386" style={{ transform: 'rotate(180deg)' }} />
+                    </div>
+                    <Select label="Куда" className="font-bold">
+                        <Option className="font-bold">Material Tailwind HTML</Option>
+                        <Option className="font-bold">Material Tailwind React</Option>
+                        <Option className="font-bold">Material Tailwind Vue</Option>
+                        <Option className="font-bold">Material Tailwind Angular</Option>
+                        <Option className="font-bold">Material Tailwind Svelte</Option>
                     </Select>
                 </div>
             </div>
